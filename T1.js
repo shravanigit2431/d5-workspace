34796af0d808e7345d6093724706f7460ab5220a
shravanigit2431 <gaddamshravani2431@gmail.com>
2024-09-27 15:45:19 +0530

    Add files via upload

--- T1.js
+++ T1.js
@@ -1,4 +1,3 @@
-function Add(a,b){
-    return a+b;
-}
-module.exports=Add;
+let {testFunction,testFunction1} = require('./test14'); // Make sure this path is correct based on your project structure
+console.log(testFunction(10,20));
+console.log(testFunction1(20,30));
---
+++ test14.js
@@ -0,0 +1,7 @@
+function testFunction(a,b) {
+    return a + b;
+}
+function testFunction1(x,y){
+    return a * b;
+}
+module.exports = { testFunction,testFunction1}

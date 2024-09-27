6c3ef95f327b920b48c0c4fc5215767cf49aeebc
shravanigit2431 <gaddamshravani2431@gmail.com>
2024-09-27 15:46:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,4 @@
+console.log("wecome to express js");
+console.log("it is my second console screen");
+console.log("after adding nodemon library");
+console.log("adding again");       
---
+++ server1.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app=express();
+
+app.get("/getUser",(req,res)=>{
+  res.send("Welcome to getUser endpoint");
+})
+app.get("/getUser1",(req,res)=>{
+    res.send("Welcome to getUser1 endpoint");
+  })
+app.post("/getUser2",(req,res)=>{
+    res.send("Welcome to post method");
+})
+app.put("/getUser3",(req,res)=>{
+    res.send("Welcome to put method");
+})
+app.delete("/getUser4",(req,res)=>{
+    res.send("Welcome to Delete method");
+})
+
+app.listen(5500,()=>{
+    console.log("My server is on port 5500");
+})
